8b4405dfd58fb38d9b448ae1d6c1fda87cf032af
lightjedi5 <jerryplatero@hotmail.com>
2020-01-23 12:51:03 -0700

    changed the directory location of week02

---
+++ web/week03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CS313 Group 04 - Teach 03</title>
+    </head>
+    <body>
+
+
+        <main>
+            <h1> CS313 Group 04 - Teach 03</h1>
+            <form>
+                <input type
+
+                
+            </form>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
