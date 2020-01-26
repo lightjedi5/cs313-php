6dd7716556490eec696a2159d69611c6b77ee901
lightjedi5 <jerryplatero@hotmail.com>
2020-01-25 23:21:23 -0700

    changing stuff in shopping cart view

--- web/week03/shopping_cart.php
+++ web/week03/shopping_cart.php
@@ -51,7 +51,6 @@ function _addImage($name){
                         <div class="row">
                             <div class="col-12 col-sm-12 col-md-2 text-center">
                                     <?php _addImage($items[$_SESSION["cart"][$i]]); ?>
-                                    <!--<img class="img-responsive" src="http://placehold.it/120x80" alt="prewiew" width="120" height="80">-->
                             </div>
                             <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
                                 <h4 class="product-name"><strong><?php echo($items[$_SESSION["cart"][$i]]); ?></strong></h4>
@@ -101,7 +100,7 @@ function _addImage($name){
                     </div>
                     <div class="pull-right" style="margin: 10px">
                         <a href="browseItems.php" class="btn btn-success pull-right">Continue Shopping</a>
-                        <a href="checkout.php" class="btn btn-success pull-right">Checkout</a>
+                        <a href="checkout.html" class="btn btn-success pull-right">Checkout</a>
                         <div class="pull-right" style="margin: 5px">
                             Total price: <b><?php echo($total); ?></b>
                         </div>
