a5582631d365063eab641471b151c879530b4da9
lightjedi5 <jerryplatero@hotmail.com>
2020-01-25 22:33:56 -0700

    added shopping cart view

--- web/week03/shopping_cart.php
+++ web/week03/shopping_cart.php
@@ -19,6 +19,7 @@ function _addImage($name){
         echo "<img class='game img-responsive' src='medievil.jpg' alt='medievil'>";
         break;
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,7 +50,7 @@ function _addImage($name){
 ?>
                         <div class="row">
                             <div class="col-12 col-sm-12 col-md-2 text-center">
-                                    
+                                    <?php _addImage($items[$_SESSION["cart"][$i]]); ?>
                                     <!--<img class="img-responsive" src="http://placehold.it/120x80" alt="prewiew" width="120" height="80">-->
                             </div>
                             <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
