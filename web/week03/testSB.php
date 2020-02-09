08711b048bebf60ea5265ddc75254155736646f5
lightjedi5 <jerryplatero@hotmail.com>
2020-02-08 22:10:53 -0700

    doing stuff for assign05

--- web/week03/testSB.php
+++ web/week03/testSB.php
@@ -21,10 +21,15 @@
           echo 'Error!: ' . $ex->getMessage();
           die();
         }
-
+        try{
         $statement = $db->query('SELECT username, password FROM note_user');
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
           echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
         }
+      }
+      catch(PDOException $ex)
+      {
+        echo 'Error!: ' . $ex->getMessage();
+      }
     ?>
