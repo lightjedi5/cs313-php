2ce0ee9c5a31c8209e58b783c745bac5648af7d5
lightjedi5 <jerryplatero@hotmail.com>
2020-02-08 22:02:27 -0700

    doing stuff for assign05

--- web/week03/testSB.php
+++ web/week03/testSB.php
@@ -7,7 +7,27 @@
 </head>
 <body>
     <?php
-        include 'db_connect.php';
+        try
+        {
+          $dbUrl = getenv('DATABASE_URL');
+        
+          $dbOpts = parse_url($dbUrl);
+        
+          $dbHost = $dbOpts["host"];
+          $dbPort = $dbOpts["port"];
+          $dbUser = $dbOpts["user"];
+          $dbPassword = $dbOpts["pass"];
+          $dbName = ltrim($dbOpts["path"],'/');
+        
+          $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+          echo 'Error!: ' . $ex->getMessage();
+          die();
+        }
 
         $statement = $db->query('SELECT username, password FROM note_user');
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
