a65c240a9ee0bc48e2697351b1df9a50608324b3
lightjedi5 <jerryplatero@hotmail.com>
2020-02-08 22:11:58 -0700

    doing stuff for assign05

--- web/week03/testSB.php
+++ web/week03/testSB.php
@@ -22,7 +22,7 @@
           die();
         }
         try{
-        $statement = $db->query('SELECT username, password FROM note_user');
+        $statement = $db->query("SELECT username, password FROM note_user");
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
           echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
