5b236013566f91fa6a0986572900951bb16824f5
lightjedi5 <jerryplatero@hotmail.com>
2020-02-08 21:57:23 -0700

    doing stuff for assign05

--- web/week03/testSB.php
+++ web/week03/testSB.php
@@ -9,19 +9,11 @@
     <?php
         include 'db_connect.php';
 
-        $query = "SELECT * FROM note";
-        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-    
-        // Printing results in HTML
-        echo "<table>\n";
-        while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-            echo "\t<tr>\n";
-            foreach ($line as $col_value) {
-                echo "\t\t<td>$col_value</td>\n";
-            }
-            echo "\t</tr>\n";
+        $statement = $db->query('SELECT username, password FROM note_user');
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+          echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
         }
-        echo "</table>\n";
     ?>
 </body>
 </html>
