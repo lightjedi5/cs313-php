6f645e019d303ee42eb9fa89d57b3584e4795f72
lightjedi5 <jerryplatero@hotmail.com>
2020-02-08 21:43:34 -0700

    doing stuff for assign05

---
+++ web/week03/testSB.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'db_connect.php';
+
+        $query = "SELECT * FROM note";
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    
+        // Printing results in HTML
+        echo "<table>\n";
+        while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+            echo "\t<tr>\n";
+            foreach ($line as $col_value) {
+                echo "\t\t<td>$col_value</td>\n";
+            }
+            echo "\t</tr>\n";
+        }
+        echo "</table>\n";
+    ?>
+</body>
+</html>
